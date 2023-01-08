36abd1374ac843ceecfd5d962e2cb0763bcd5e3c
Murat Yirci <myirci@gmail.com>
2023-01-08 11:58:42 -0600

    Adapt the solution to path changes.

--- TestProject/TestConfig.hpp
+++ TestProject/TestConfig.hpp
@@ -2,5 +2,5 @@
 
 #include <string_view>
 
-inline constexpr std::string_view input_img_path = "C:/Users/myirc/source/repos/github/simple_image_library/data/input";
-inline constexpr std::string_view output_img_path = "C:/Users/myirc/source/repos/github/simple_image_library/data/output";
+inline constexpr std::string_view input_img_path = "C:/Users/myirc/source/repos/github/image_lib/Data/Input";
+inline constexpr std::string_view output_img_path = "C:/Users/myirc/source/repos/github/image_lib/Data/Output";
